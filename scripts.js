9346a7205e8e6e9c4bac40115707b383608fe8d4
natanree <natanreee@gmail.com>
2026-07-21 16:00:49 -0700

    new scripts.js to replace import.js

---
+++ scripts.js
@@ -0,0 +1,29 @@
+let mangaList = [];
+
+const input = document.getElementById('fileInput');
+
+input.addEventListener('change', async (event) => {
+    const file = event.target.files[0];
+
+    if (!file) {
+        return;
+    }
+
+    const text =  await file.text();
+    mangaList = JSON.parse(text).series;
+
+    renderTable(mangaList);
+})
+
+function renderTable(data) {
+    const tableBody = document.getElementById('seriesTableBody');
+    tableBody.innerHTML = '';
+    data.forEach((series) => {
+        const row = document.createElement('tr');
+
+        row.innerHTML = `<td>${series.name}</td>
+                        <td>${series.current_chapter}</td>
+                        <td>${series.status}</td>`;
+        tableBody.appendChild(row);
+    });
+}

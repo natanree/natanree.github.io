c5f5e3e6ed4321ea5917240ac06b008602f99840
natanree <natanreee@gmail.com>
2026-07-21 15:45:03 -0700

    added table rendering

--- import.js
+++ import.js
@@ -10,5 +10,18 @@ input.addEventListener('change', async (event) => {
     const text =  await file.text();
     const data = JSON.parse(text);
 
-    console.log(data);
+    renderTable(data.series);
 })
+
+function renderTable(data) {
+    const tableBody = document.getElementById('seriesTableBody');
+    tableBody.innerHTML = '';
+    data.forEach((series) => {
+        const row = document.createElement('tr');
+
+        row.innerHTML = `<td>${series.name}</td>
+                        <td>${series.current_chapter}</td>
+                        <td>${series.status}</td>`;
+        tableBody.appendChild(row);
+    });
+}
